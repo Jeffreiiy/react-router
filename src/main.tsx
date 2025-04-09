e11cf6de4f91691b5f28ef3bef9385e8fed8c854
vianelbn <vb.nyamsi@yahoo.fr>
2025-04-09 12:21:47 +0200

    last commit react-router

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,43 @@ import App from "./App";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Article from "./pages/Article";
 
 // router creation
+const getWeatherOfTheDay = () => {
+	return "sunny";
+};
+const allData = [
+	{
+		id: 1,
+		title: "Lorem Ipsum",
+		content: "Lorem ipsum dolor sit amet",
+	},
+	{
+		id: 2,
+		title: "Schnapsum",
+		content: "Lorem Elsass ipsum Salut bisamme",
+	},
+	{
+		id: 3,
+		title: "Cupcake Ipsum",
+		content: "Tiramisu pastry wafer brownie soufflé",
+	},
+];
+
+type Data = (typeof allData)[0];
+
+const getSomeData = (id: number) => {
+	return allData.find((article) => article.id === id) as Data | null;
+};
 
 const router = createBrowserRouter([
 	{
 		element: <App />,
+		loader: () => {
+			return getWeatherOfTheDay();
+		},
+		id: "app",
 		children: [
 			{
 				path: "/",
@@ -21,6 +52,15 @@ const router = createBrowserRouter([
 			{
 				path: "/about",
 				element: <About />,
+			},
+			{
+				path: "/articles/:id",
+				element: <Article />,
+				loader: ({ params }) => {
+					const idAsInt = Number.parseInt(params.id ?? "0");
+
+					return getSomeData(idAsInt);
+				},
 			},
 		],
 	},
